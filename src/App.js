b22879b1eabce927d51478e248c45e19d4fd7f26
jeeyawn <jiony432@uw.edu>
2020-10-18 17:03:17 -0500

    edit package.json for deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import './App.css';
 
 import NavigationBar from "./components/navigationbar.component";

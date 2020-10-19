090dee6ac25d931a2efeb9a8128b047c4e29d279
jeeyawn <jiony432@uw.edu>
2020-10-19 09:48:16 -0500

    edit profile img size

--- src/components/navigationbar.component.js
+++ src/components/navigationbar.component.js
@@ -18,7 +18,7 @@ export default class NavigationBar extends Component {
                  >
                     <Navbar.Brand>
                         <Nav.Link href='/'>
-                            <Image src={profile} roundedCircle fluid style={{'height':'5em'}}/>
+                            <Image src={profile} roundedCircle style={{'height':'5em'}}/>
                         </Nav.Link>
                     </Navbar.Brand>
 
